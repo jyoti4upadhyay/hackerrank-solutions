f027123788a6886505f3c950ce8e65dd0298e580
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-04 02:07:40 +0530

    ✅ Arrays: Left Rotation [java8] — approach1

---
+++ solutions/java8/easy/ctci-array-left-rotation/solution-approach1.java
@@ -0,0 +1,105 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?isFullScreen=true
+// Problem     Arrays: Left Rotation
+// Difficulty  Easy
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-04, 02:07 a.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'rotLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER d
+     */
+
+    public static List<Integer> rotLeft(List<Integer> a, int d) {
+        int size=a.size();
+    // Write your code here
+    int arr[]=new int[size]; 
+    for(int i=0;i<size;i++){
+        arr[i]=a.get(i);
+    }
+    //rev whole arr
+    int rev[] =new int[size];
+    int k=0;
+    for(int j=size-1;j>=0;j--){
+        rev[k]=arr[j];
+        k++;
+    }
+    
+    //first half rev (size-d)
+    int rev1[]=new int [size-d];
+    int l=0;
+    for(int p=size-d-1;p>=0;p--){
+        rev1[l]=rev[p];
+        l++;
+    }
+    //next half
+    int rev2[]=new int[d];
+    l=0;
+    for(int q=size-1;q>=size-d;q--){
+        rev2[l]=rev[q];
+        l++;
+    }
+    //
+    List<Integer> ls=new ArrayList<>();
+    for(int i=0;i<rev1.length;i++){
+        ls.add(rev1[i]);
+    }
+    for(int j=0;j<rev2.length;j++){
+        ls.add(rev2[j]);
+    }
+    
+    return ls;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.rotLeft(a, d);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
