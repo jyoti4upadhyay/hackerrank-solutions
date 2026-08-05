2419729faf4f77851dd6bc37630729daf7fc8d22
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-05 23:29:37 +0530

    ✅ Two Strings [java] — approach1

---
+++ solutions/java/easy/two-strings/solution-approach1.java
@@ -0,0 +1,67 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/two-strings/problem?isFullScreen=true
+// Problem     Two Strings
+// Difficulty  Easy
+// Subdomain   Strings
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-05, 11:29 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static String twoStrings(String s1, String s2) {
+    // Write your code here
+    HashSet<Character>hs=new HashSet<>();
+    for(int i=0;i<s1.length();i++){
+        hs.add(s1.charAt(i));
+    }
+    for(int i=0;i<s2.length();i++){
+        if(hs.contains(s2.charAt(i))){
+            return "YES";
+        }
+    }
+    return "NO";
+    
+}
+}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = bufferedReader.readLine();
+
+            String s2 = bufferedReader.readLine();
+
+            String result = Result.twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
