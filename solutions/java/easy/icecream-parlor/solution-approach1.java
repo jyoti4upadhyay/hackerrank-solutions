5dff8f4b70d89bf0fc9abff0033495716fbe883c
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-05 16:27:57 +0530

    ✅ Ice Cream Parlor [java] — approach1

---
+++ solutions/java/easy/icecream-parlor/solution-approach1.java
@@ -0,0 +1,86 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/icecream-parlor/problem?isFullScreen=true
+// Problem     Ice Cream Parlor
+// Difficulty  Easy
+// Subdomain   Search
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-05, 04:27 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'icecreamParlor' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER m
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> icecreamParlor(int m, List<Integer> arr) {
+    // Write your code here
+    
+    List<Integer> ls=new ArrayList<>();
+    for(int i=0;i<arr.size()-1;i++){
+        for(int j=i+1;j<arr.size();j++){
+            int sum=arr.get(i)+arr.get(j);
+            if(sum==m){
+                ls.add(i+1);
+                ls.add(j+1);
+            }
+        }
+    }
+    return ls;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arr = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrTemp[i]);
+                arr.add(arrItem);
+            }
+
+            List<Integer> result = Result.icecreamParlor(m, arr);
+
+            for (int i = 0; i < result.size(); i++) {
+                bufferedWriter.write(String.valueOf(result.get(i)));
+
+                if (i != result.size() - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
