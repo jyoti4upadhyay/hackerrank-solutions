9529021a6c798d073e81c7fa036ca852d7708974
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-09 01:24:20 +0530

    ✅ Sales by Match [java] — approach1

---
+++ solutions/java/easy/sock-merchant/solution-approach1.java
@@ -0,0 +1,80 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true
+// Problem     Sales by Match
+// Difficulty  Easy
+// Subdomain   Implementation
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-09, 01:24 a.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+    // Write your code here
+    int pair=0;
+    int[] arr=new int[ar.size()];
+    for(int i=0;i<ar.size();i++){
+        arr[i]=ar.get(i);
+    }
+    Arrays.sort(arr);
+    int i=0;
+    while(i<n-1){
+        if(arr[i]==arr[i+1]){
+            pair++;
+            i=i+2;  
+        }
+        else {
+            i=i+1;
+        }
+    }
+    return pair;
+    
+    }
+
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
