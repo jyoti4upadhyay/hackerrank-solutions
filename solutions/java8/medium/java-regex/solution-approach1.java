42fb3193ad5d1d47c9b4ad268c64dc2da8db30c5
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-10 15:46:19 +0530

    ✅ Java Regex [java8] — approach1

---
+++ solutions/java8/medium/java-regex/solution-approach1.java
@@ -0,0 +1,17 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-regex/problem?isFullScreen=true
+// Problem     Java Regex
+// Difficulty  Medium
+// Subdomain   Strings
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-10, 03:44 p.m.
+// ──────────────────────────────────────────────────
+
+
+class MyRegex{
+    String pattern="(([0-9]|[0-9][0-9]|0[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[0-9][0-9]|0[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+   
+    
+}
