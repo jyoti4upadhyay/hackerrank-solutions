7b19c6103d95028e85a3a9a0e67e5dadb1457111
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-18 11:24:26 +0530

    ✅ Service Lane [java] — approach1

---
+++ solutions/java/easy/service-lane/solution-approach1.java
@@ -0,0 +1,102 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/service-lane/problem?isFullScreen=true
+// Problem     Service Lane
+// Difficulty  Easy
+// Subdomain   Implementation
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-18, 11:24 a.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'serviceLane' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY cases
+     */
+
+    public static List<Integer> serviceLane(int n, List<List<Integer>> cases,List<Integer> width) {
+    // Write your code here
+    List<Integer>res=new ArrayList<>();
+   
+    for(int i=0;i<cases.size();i++){
+        int min=Integer.MAX_VALUE;
+        int start=cases.get(i).get(0);
+        int end=cases.get(i).get(1);
+        while(start<=end && end<width.size()){
+            if(width.get(start)<min){
+                min=width.get(start);  
+            }
+            start++;       
+        }
+        res.add(min);
+    }
+    return res;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int t = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] widthTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> width = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int widthItem = Integer.parseInt(widthTemp[i]);
+            width.add(widthItem);
+        }
+
+        List<List<Integer>> cases = new ArrayList<>();
+
+        for (int i = 0; i < t; i++) {
+            String[] casesRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> casesRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 2; j++) {
+                int casesItem = Integer.parseInt(casesRowTempItems[j]);
+                casesRowItems.add(casesItem);
+            }
+
+            cases.add(casesRowItems);
+        }
+
+        List<Integer> result = Result.serviceLane(n, cases,width);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
