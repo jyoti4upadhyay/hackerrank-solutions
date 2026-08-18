25ac608e3b6ff07fde3679e146f0d5b6f98787ca
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-18 15:32:16 +0530

    ✅ Java Inheritance I [java] — approach1

---
+++ solutions/java/easy/java-inheritance-1/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-inheritance-1/problem?isFullScreen=true
+// Problem     Java Inheritance I
+// Difficulty  Easy
+// Subdomain   Object Oriented Programming
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-18, 03:32 p.m.
+// ──────────────────────────────────────────────────
+
+
+
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+    void sing(){
+                System.out.println("I am singing");
+    }
+}
+
+
