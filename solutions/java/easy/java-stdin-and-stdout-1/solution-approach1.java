eeb0d2b0ca410d294998dbc87638af3783bf10da
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-19 11:01:52 +0530

    ✅  Java Stdin and Stdout I [java] — approach1

---
+++ solutions/java/easy/java-stdin-and-stdout-1/solution-approach1.java
@@ -0,0 +1,24 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
+// Problem      Java Stdin and Stdout I
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-19, 11:01 a.m.
+// ──────────────────────────────────────────────────
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int num=sc.nextInt();
+            System.out.println(num);
+        }
+       
+    }
+}
