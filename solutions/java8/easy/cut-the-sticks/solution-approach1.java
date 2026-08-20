2e8c69e94e353672126b310791f721a1072ec585
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-20 16:49:23 +0530

    ✅ Cut the sticks [java8] — approach1

---
+++ solutions/java8/easy/cut-the-sticks/solution-approach1.java
@@ -0,0 +1,79 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/cut-the-sticks/problem?isFullScreen=true
+// Problem     Cut the sticks
+// Difficulty  Easy
+// Subdomain   Implementation
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-20, 04:49 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'cutTheSticks' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> cutTheSticks(List<Integer> arr) {
+    // Write your code here
+    Collections.sort(arr);
+    List<Integer>ls=new ArrayList<>();
+    for(int i=0;i<arr.size();i++){
+        if(i>0 && arr.get(i).equals(arr.get(i-1))){
+            continue;
+        }
+        List<Integer>res=new ArrayList<>(arr);
+        int count=0;
+            for(int j=0;j<res.size();j++){
+                int diff=res.get(j)-arr.get(i);
+                if(diff<0){
+                    count++;      
+                }   
+            }   
+        ls.add(arr.size()-count);    
+    }
+    return ls;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.cutTheSticks(arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
