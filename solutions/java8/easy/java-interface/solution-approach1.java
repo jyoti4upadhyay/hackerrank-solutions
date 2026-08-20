954d8d9d8c81914e4216da595768f32aa7e3b9c9
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-20 14:24:22 +0530

    ✅ Java Interface [java8] — approach1

---
+++ solutions/java8/easy/java-interface/solution-approach1.java
@@ -0,0 +1,25 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-interface/problem?isFullScreen=true
+// Problem     Java Interface
+// Difficulty  Easy
+// Subdomain   Object Oriented Programming
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-20, 02:24 p.m.
+// ──────────────────────────────────────────────────
+
+
+class MyCalculator implements AdvancedArithmetic{
+    
+    public int divisor_sum(int n){
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                sum=sum+i;
+            }
+        } 
+        return sum;   
+    }
+}
+
