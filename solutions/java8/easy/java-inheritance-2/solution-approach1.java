fbaf96d556ad2b8739e736d18149630895ea8918
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-20 14:19:21 +0530

    ✅ Java Inheritance II [java8] — approach1

---
+++ solutions/java8/easy/java-inheritance-2/solution-approach1.java
@@ -0,0 +1,22 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-inheritance-2/problem?isFullScreen=true
+// Problem     Java Inheritance II
+// Difficulty  Easy
+// Subdomain   Object Oriented Programming
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-20, 02:19 p.m.
+// ──────────────────────────────────────────────────
+
+
+
+class Arithmetic{
+    public int add(int a ,int b){
+        return a+b;
+    }
+}
+class Adder extends Arithmetic{
+     
+}
+
