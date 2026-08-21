c9389c65d350459bf1789623a10c2e13997b45ca
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-21 15:14:12 +0530

    ✅ Java 1D Array [java] — approach1

---
+++ solutions/java/easy/java-1d-array-introduction/solution-approach1.java
@@ -0,0 +1,16 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-1d-array-introduction/problem?isFullScreen=true
+// Problem     Java 1D Array
+// Difficulty  Easy
+// Subdomain   Data Structures
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-21, 03:13 p.m.
+// ──────────────────────────────────────────────────
+
+
+        int[] a=new int[n];
+        for(int i=0;i<a.length;i++){
+            a[i]=scan.nextInt();
+        }
