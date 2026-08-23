2eb4d72c4ea853e8f5e88504b2ac394320ff0a43
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-23 12:21:49 +0530

    ✅ Valid Username Regular Expression [java] — approach1

---
+++ solutions/java/easy/valid-username-checker/solution-approach1.java
@@ -0,0 +1,19 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/valid-username-checker/problem?isFullScreen=true
+// Problem     Valid Username Regular Expression
+// Difficulty  Easy
+// Subdomain   Strings
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-23, 12:21 p.m.
+// ──────────────────────────────────────────────────
+
+
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression ="^[A-Za-z][a-zA-Z0-9_]{7,29}$";
+}
+
