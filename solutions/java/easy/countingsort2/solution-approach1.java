5b5ebb9dafe89954e0b7dfa81428fe6e2b0c9944
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-08-23 23:53:22 +0530

    ✅ Counting Sort 2 [java] — approach1

---
+++ solutions/java/easy/countingsort2/solution-approach1.java
@@ -0,0 +1,78 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/countingsort2/problem?isFullScreen=true
+// Problem     Counting Sort 2
+// Difficulty  Easy
+// Subdomain   Sorting
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-23, 11:53 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        List<Integer>ls=new ArrayList<>();
+        int[] num=new int[100];
+        for(int i=0;i<arr.size();i++){
+            num[arr.get(i)]++; 
+        }
+        
+        for(int i=0;i<num.length;i++){
+            for(int j=0;j<num[i];j++){
+                ls.add(i);
+            }
+        }
+        return ls;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        List<Integer> result = Result.countingSort(arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
