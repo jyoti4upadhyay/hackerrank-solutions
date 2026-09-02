02509e5c4f8ffc7c60e9289f5391328c09f60014
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-09-02 10:44:57 +0530

    ✅ Matching Digits & Non-Digit Characters [java8] — approach1

---
+++ solutions/java8/easy/matching-digits-non-digit-character/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/matching-digits-non-digit-character/problem?isFullScreen=true
+// Problem     Matching Digits & Non-Digit Characters
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-02, 10:44 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\d{2}\\D\\d{2}\\D\\d{4}"); // Use \\ instead of using \ 
+    
+    }
+}
+
