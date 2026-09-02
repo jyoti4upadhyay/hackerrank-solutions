a19113aaa809922c727a6cf434fbcd0566744321
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-09-02 10:53:58 +0530

    ✅ Matching {x, y} Repetitions [java8] — approach1

---
+++ solutions/java8/easy/matching-x-y-repetitions/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/matching-x-y-repetitions/problem?isFullScreen=true
+// Problem     Matching {x, y} Repetitions
+// Difficulty  Easy
+// Subdomain   Repetitions
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-02, 10:53 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[\\d]{1,2}[A-Za-z]{3,}(\\.){0,3}$"); // Use \\ instead of using \ 
+    
+    }
+}
+
