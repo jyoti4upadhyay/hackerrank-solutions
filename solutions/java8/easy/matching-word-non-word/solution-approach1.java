11a20d94a391a7114bb38a13e0dc57f5475909e7
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-09-02 10:49:39 +0530

    ✅ Matching Word & Non-Word Character [java8] — approach1

---
+++ solutions/java8/easy/matching-word-non-word/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/matching-word-non-word/problem?isFullScreen=true
+// Problem     Matching Word & Non-Word Character
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-02, 10:49 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(\\w){3}(\\W)(\\w){10}(\\W)(\\w){3}"); // Use \\ instead of using \ 
+    
+    }
+}
+
