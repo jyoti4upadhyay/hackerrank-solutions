ce9cc1d75eeb612e7248e143ae0426dcfa18b2bd
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-09-02 10:55:17 +0530

    ✅ Matching Zero Or More Repetitions [java8] — approach1

---
+++ solutions/java8/easy/matching-zero-or-more-repetitions/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/matching-zero-or-more-repetitions/problem?isFullScreen=true
+// Problem     Matching Zero Or More Repetitions
+// Difficulty  Easy
+// Subdomain   Repetitions
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-02, 10:55 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^\\d{2,}[a-z]*[A-Z]*$"); // Use \\ instead of using \ 
+    
+    }
+}
+
