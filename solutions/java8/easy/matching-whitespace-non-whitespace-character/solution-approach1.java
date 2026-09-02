637ede80d965019b53522f4091c91e4110696e9a
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-09-02 10:47:58 +0530

    ✅ Matching Whitespace & Non-Whitespace Character [java8] — approach1

---
+++ solutions/java8/easy/matching-whitespace-non-whitespace-character/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/matching-whitespace-non-whitespace-character/problem?isFullScreen=true
+// Problem     Matching Whitespace & Non-Whitespace Character
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-02, 10:47 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(\\S){2}(\\s)(\\S){2}(\\s)(\\S){2}"); // Use \\ instead of using \ 
+
+    }
+}
+
