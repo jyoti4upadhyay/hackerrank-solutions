aa3b56f4e15bc2d44e7f1781fef4e11462f1784f
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-09-02 10:58:34 +0530

    ✅ Matching Ending Items [java8] — approach1

---
+++ solutions/java8/easy/matching-ending-items/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/matching-ending-items/problem?isFullScreen=true
+// Problem     Matching Ending Items
+// Difficulty  Easy
+// Subdomain   Repetitions
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-02, 10:58 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[A-Za-z]*s$"); // Use \\ instead of using \ 
+    
+    }
+}
+
