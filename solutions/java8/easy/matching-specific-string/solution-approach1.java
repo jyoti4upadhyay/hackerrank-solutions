5739e894b68a909bd4be6cd50f48c1fc6d8b40cf
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-09-02 10:18:20 +0530

    ✅ Matching Specific String [java8] — approach1

---
+++ solutions/java8/easy/matching-specific-string/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/matching-specific-string/problem?isFullScreen=true
+// Problem     Matching Specific String
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-02, 10:18 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(hackerrank)"); 
+    
+    }
+}
+
