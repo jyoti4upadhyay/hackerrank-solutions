0357a9366c54b3e27fba5ce0539bb091173e9e80
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-09-02 10:56:24 +0530

    ✅ Matching One Or More Repetitions [java8] — approach1

---
+++ solutions/java8/easy/matching-one-or-more-repititions/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/matching-one-or-more-repititions/problem?isFullScreen=true
+// Problem     Matching One Or More Repetitions
+// Difficulty  Easy
+// Subdomain   Repetitions
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-02, 10:56 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^\\d{1,}[A-Z]{1,}[a-z]{1,}$"); // Use \\ instead of using \ 
+    
+    }
+}
+
