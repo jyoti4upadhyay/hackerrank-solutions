ba2c762e055ada0e746f7aee08d8f3c74bcbf7cd
Jyoti Upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2026-09-02 10:52:13 +0530

    ✅ Matching Start & End [java8] — approach1

---
+++ solutions/java8/easy/matching-start-end/solution-approach1.java
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/matching-start-end/problem?isFullScreen=true
+// Problem     Matching Start & End
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-02, 10:51 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^\\d[\\w]{4}\\.$"); // Use \\ instead of using \ 
+    
+    }
+}
+
